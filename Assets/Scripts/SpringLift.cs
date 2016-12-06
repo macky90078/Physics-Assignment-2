3174f13c1b93d1b45114db2f485c26be0d8fda06
Mackenzie Wilson <macky90078@gmail.com>
2016-12-06 15:20:56 -0500

    did some work on roap swing, and got started on the spring lift. also edited the level a bit.

--- Assets/Scripts/SpringLift.cs
+++ Assets/Scripts/SpringLift.cs
@@ -1,24 +1,27 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Spring : MonoBehaviour {
+public class SpringLift : MonoBehaviour {
 
     private GameObject m_hook;
     private SpringJoint m_springJoint;
     private Vector3 m_savePos;
+    private Rigidbody m_collidingRb;
+
     public bool m_isPlayerConnected = false;
 
     void OnTriggerEnter(Collider col)
-	{
-		Rigidbody CollidingRb = col.gameObject.GetComponent<Rigidbody> ();
+    {
+        m_collidingRb = col.gameObject.GetComponent<Rigidbody>();
 
-		if (CollidingRb)
-		{
-			 
-			Vector3 yOffset = new Vector3 (0.0f, 0.6f, 0.0f);
-			m_hook.transform.position = col.gameObject.transform.position + yOffset;
-			m_hook.transform.SetParent (col.gameObject.transform);
-			m_springJoint.connectedBody = CollidingRb;
+        if (m_collidingRb)
+        {
+
+            Vector3 yOffset = new Vector3(0.0f, 0.6f, 0.0f);
+            m_hook.transform.position = col.gameObject.transform.position + yOffset;
+            m_hook.transform.SetParent(col.gameObject.transform);
+            m_springJoint.connectedBody = m_collidingRb;
             CharacterController controller = col.gameObject.GetComponent<CharacterController>();
             if (controller)
             {
@@ -26,12 +29,12 @@ public class Spring : MonoBehaviour {
                 m_isPlayerConnected = true;
             }
         }
-	}
+    }
 
     void OnDisconnect()
     {
         GameObject connectedPlayer = m_springJoint.connectedBody.gameObject;
-        Transform child = connectedPlayer.transform.GetChild(0);
+        Transform child = connectedPlayer.transform.GetChild(1);
         child.parent = null;
         Rigidbody ogHook = child.gameObject.GetComponent<Rigidbody>();
         m_springJoint.connectedBody = ogHook;
@@ -45,25 +48,27 @@ public class Spring : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
-
-		m_springJoint = GetComponent<SpringJoint> ();
-	    m_hook = m_springJoint.connectedBody.gameObject;
+    void Start()
+    {
+        m_springJoint = GetComponent<SpringJoint>();
+        m_hook = m_springJoint.connectedBody.gameObject;
         m_savePos = m_hook.transform.position;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
+    // Update is called once per frame
+    void Update()
+    {
         if (m_isPlayerConnected)
         {
+            m_collidingRb.AddForce(30 * m_hook.transform.up);
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 OnDisconnect();
                 m_hook.transform.position = m_savePos;
-                
+
             }
         }
 
     }
 }
+
